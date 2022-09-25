27a0590c7f2645c80a5bbf8eaee237b591858ab7
PHSC138 <PHSC138@users.noreply.github.com>
2022-09-24 23:44:29 -0400

    Test key

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
     next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
@@ -40,7 +40,7 @@ app.use(function (err, req, res, next) {
     // res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.locals.error = {};
 
-    // render the error page
+    // Render the error page
     res.status(err.status || 500);
     res.render('error', {title: "Error"});
 });
